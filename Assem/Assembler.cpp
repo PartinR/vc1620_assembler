92af0c49c241ac9c4421d7ced39e9c973b9edf98
Race Partin <racepartin@gmail.com>
2025-11-06 20:09:48 -0500

    docs: move Assembler documentation to .h file

--- Assem/Assembler.cpp
+++ Assem/Assembler.cpp
@@ -7,15 +7,6 @@
 #include "Instruction.h"
 #include "Errors.h"
 
-/// <summary>
-/// Initializes the Assembler class, setting up the necessary components for assembly processing. 
-/// This constructor passes the application's command-line arguments (<paramref name="argc"/> and 
-/// <paramref name="argv"/>) directly to the internal file access component (m_facc) for initialization.
-/// </summary>
-/// <param name="argc">The number of command-line arguments provided to the main program.</param>
-/// <param name="argv">The array of command-line argument strings provided to the main program.</param>
-/// <author>Race Partin</author>
-/// <date>11/05/2025 10:04pm</date>
 Assembler::Assembler( int argc, char *argv[] )
 : m_facc( argc, argv )
 {
@@ -27,18 +18,6 @@ Assembler::~Assembler( )
 {
 }
 
-/// <summary>
-/// Performs Pass I of the assembly process to establish the memory location of all labels.
-/// This function iterates through the source file line by line, maintaining a location counter 
-/// to track the address of the instructions being processed. Its primary role is to 
-/// populate the symbol table (m_symtab) by recording every encountered label along with 
-/// its calculated location. Lines marked as comments or the final END statement are 
-/// used to control the flow of the pass. Missing END statements are allowed in this 
-/// pass but will be reported as an error in Pass II.
-/// </summary>
-/// <returns>This function does not return a value (void).</returns>
-/// <author>Race Partin</author>
-/// <date>11/05/2025 10:08pm</date>
 void Assembler::PassI( ) 
 {
     // Tracks the location of the instructions to be generated.
--- Assem/Assembler.h
+++ Assem/Assembler.h
@@ -14,10 +14,31 @@ class Assembler {
 
 public:
 
+    /// <summary>
+    /// Initializes the Assembler class, setting up the necessary components for assembly processing. 
+    /// This constructor passes the application's command-line arguments (<paramref name="argc"/> and 
+    /// <paramref name="argv"/>) directly to the internal file access component (m_facc) for initialization.
+    /// </summary>
+    /// <param name="argc">The number of command-line arguments provided to the main program.</param>
+    /// <param name="argv">The array of command-line argument strings provided to the main program.</param>
+    /// <author>Race Partin</author>
+    /// <date>11/05/2025 10:04pm</date>
     Assembler( int argc, char *argv[] );
+
     ~Assembler( );
 
-    // Pass I - establish the locations of the symbols
+    /// <summary>
+    /// Performs Pass I of the assembly process to establish the memory location of all labels.
+    /// This function iterates through the source file line by line, maintaining a location counter 
+    /// to track the address of the instructions being processed. Its primary role is to 
+    /// populate the symbol table (m_symtab) by recording every encountered label along with 
+    /// its calculated location. Lines marked as comments or the final END statement are 
+    /// used to control the flow of the pass. Missing END statements are allowed in this 
+    /// pass but will be reported as an error in Pass II.
+    /// </summary>
+    /// <returns>This function does not return a value (void).</returns>
+    /// <author>Race Partin</author>
+    /// <date>11/05/2025 10:08pm</date>
     void PassI( );
 
     // Pass II - generate a translation
