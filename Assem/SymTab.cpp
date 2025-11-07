418ade8ecc8a79df1521f068481a929e75aaf4c4
Race Partin <racepartin@gmail.com>
2025-11-06 22:03:51 -0500

    feat: implement LookupSymbol function

--- Assem/SymTab.cpp
+++ Assem/SymTab.cpp
@@ -10,12 +10,33 @@ void SymbolTable::AddSymbol( const string &a_symbol, int a_loc )
     // If the symbol is already in the symbol table, record it as multiply defined.
     map<string, int>::iterator st = m_symbolTable.find(a_symbol);
 
-    if(st != m_symbolTable.end()) {
-
+    if(st != m_symbolTable.end()) 
+    {
         st->second = multiplyDefinedSymbol;
         return;
     }
 
     // Record a the location in the symbol table.
     m_symbolTable[a_symbol] = a_loc;
+}
+
+bool SymbolTable::LookupSymbol( const string& a_symbol, int& a_loc )
+{
+    // TODO: Implement symbol table display logic here.
+    auto it = m_symbolTable.find(a_symbol);
+
+    if (it != m_symbolTable.end())
+    {
+        a_loc = it->second;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void SymbolTable::DisplaySymbolTable()
+{
+    // TODO: Implement this function
 }
