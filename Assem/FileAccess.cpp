45e1718342507cb560ca07ac7d1c651504aa8599
Race Partin <racepartin@gmail.com>
2025-11-06 22:15:12 -0500

    docs: clean up spacing and brackets

--- Assem/FileAccess.cpp
+++ Assem/FileAccess.cpp
@@ -9,18 +9,20 @@
 FileAccess::FileAccess( int argc, char *argv[] )
 {
     // Check that there is exactly one run time parameter.
-    if(argc != 2) {
+    if(argc != 2)
+    {
         cerr << "Usage: Assem <FileName>" << endl;
         exit(1);
     }
+
     // Open the file.  One might question if this is the best place to open the file.
     // One might also question whether we need a file access class.
     m_sfile.open(argv[1], ios::in);
 
     // If the open failed, report the error and terminate.
-    if(! m_sfile) {
-        cerr << "Source file could not be opened, assembler terminated."
-            << endl;
+    if(! m_sfile) 
+    {
+        cerr << "Source file could not be opened, assembler terminated." << endl;
         exit(1); 
     }
 }
@@ -35,10 +37,11 @@ FileAccess::~FileAccess( )
 bool FileAccess::GetNextLine( string &a_line )
 {
     // If there is no more data, return false.
-    if(m_sfile.eof()) {
-    
+    if(m_sfile.eof()) 
+    {
         return false;
     }
+
     getline(m_sfile, a_line);
     
     // Return indicating success.
