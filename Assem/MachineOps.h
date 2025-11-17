3fcf5a980e980eb075e933ea5c15225b6391f024
Race Partin <racepartin@gmail.com>
2025-11-16 21:29:09 -0500

    build: create machine operators header file

---
+++ Assem/MachineOps.h
@@ -0,0 +1,26 @@
+//
+//  Class to store machine operators.
+//
+#pragma once
+
+#include "stdafx.h"
+#include <unordered_map>
+
+class MachineOps {
+
+public:
+
+    MachineOps( ) { };
+
+    /// <summary>
+    /// Get a constant reference to the opcode.
+    /// </summary>
+    /// <returns>Returns constant reference to the unordered_map storing opcodes.</returns>
+    /// /// <author>Race Partin</author>
+    /// <date>11/16/2025 9:28pm</date.
+    const unordered_map<string, int>& GetOpcode( ) const;
+
+private:
+
+    unordered_map<string, int> m_opcode;
+};
